98f1c875623c07e05c4fc591b8c7096937ed2e68
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-10-05 20:12:42 +0530

    make files

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+// install mongodb
+//install mongoosejs
+//require and setup connection
+//make schema
+//create model and export
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
